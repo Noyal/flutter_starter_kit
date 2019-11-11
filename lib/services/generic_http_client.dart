f8bd90fefe990333216d0cf2005a9f5729bf3f70
noyalpv <noyalvincy@gmail.com>
2019-11-11 16:57:15 +0530

    add an error interceptor

--- lib/services/generic_http_client.dart
+++ lib/services/generic_http_client.dart
@@ -9,102 +9,24 @@ import 'package:starter_kit/models/enums/http_client_status_code.dart';
 import 'package:starter_kit/models/maps/http_map.dart';
 
 class HttpClient {
-  Dio dio = Dio(BaseOptions(
-    contentType: ContentType.json,
-    connectTimeout: 10000,
-    receiveTimeout: 10000,
-  ));
+  Dio dio;
   List<HttpClientStatusCode> get _success => [
         HttpClientStatusCode.OK,
         HttpClientStatusCode.CREATED,
         HttpClientStatusCode.NO_CONTENT
       ];
 
-  T _parseJson<T>(String data, {bool isArray}) {
-    return isArray ? fromJsonArrayString<T>(data) : fromJsonString<T>(data);
-  }
-
-  Future<HttpClientModel> _buildClientModel<T>(
-      Response response, HttpClientModel responseModel, bool isArray) async {
-    if (_success.contains(httpErrorVal[response.statusCode])) {
-      responseModel = HttpClientModel<T>(
-          error: HttpClientStatusCode.NONE,
-          model: await compute<String, T>(
-              (responseData) => _parseJson<T>(responseData, isArray: isArray),
-              response.data));
-    } else {
-      responseModel = HttpClientModel(
-        error: httpErrorVal[response?.statusCode] ?? HttpClientStatusCode.ERROR,
-      );
-    }
-    return responseModel;
-  }
-
-  Future<HttpClientModel<T>> get<T>(String url) => _baseGet(url);
-
-  Future<HttpClientModel<T>> getAll<T>(String url) =>
-      _baseGet(url, isArray: true);
-
-  Future<HttpClientModel<T>> _baseGet<T>(String url,
-      {bool isArray = false}) async {
-    HttpClientModel<T> responseModel;
-    try {
-      Response response = await dio.get(url);
-      responseModel =
-          await _buildClientModel<T>(response, responseModel, isArray);
-    } on DioError catch (e) {
-      responseModel = onGetError(e);
-    }
-    return responseModel;
-  }
-
-  Future<HttpClientModel<T>> post<T>(T data, String url, {bool isArray}) async {
-    HttpClientModel<T> responseModel;
-    try {
-      Response response = await dio.post(url, data: data);
-      responseModel =
-          await _buildClientModel<T>(response, responseModel, isArray);
-    } on DioError catch (e) {
-      responseModel = onGetError(e);
-    }
-    return responseModel;
-  }
-
-  Future<HttpClientStatusCode> put<T>(T data, String url) async {
-    try {
-      Response response = await dio.put(url, data: data);
-      if (_success.contains(httpErrorVal[response.statusCode]))
-        return httpErrorVal[response.statusCode];
-      else
-        throw DioError();
-    } on DioError catch (e) {
-      return _onError(e);
-    }
-  }
-
-  Future<HttpClientStatusCode> delete<T>(T data, String url) async {
-    try {
-      Response response = await dio.delete(url, data: data);
-      if (_success.contains(httpErrorVal[response.statusCode]))
-        return httpErrorVal[response.statusCode];
-      else
-        throw DioError();
-    } on DioError catch (e) {
-      return _onError(e);
-    }
-  }
-
-  HttpClientModel onGetError(DioError e) => HttpClientModel(error: _onError(e));
-
-  FutureOr<HttpClientStatusCode> _onError(DioError e) async {
-    if (e.error.runtimeType == HttpException) return await _checkConnectivity();
-    if (e.error.runtimeType == SocketException)
-      return HttpClientStatusCode.NO_INTERNET;
-    if (e.type == DioErrorType.RECEIVE_TIMEOUT)
-      return HttpClientStatusCode.SLOW_SERVER;
-    if (e.type == DioErrorType.CONNECT_TIMEOUT)
-      return HttpClientStatusCode.SLOW_INTERNET;
-    return httpErrorVal[e.response?.statusCode] ?? HttpClientStatusCode.ERROR;
+  HttpClient() {
+    dio = Dio(BaseOptions(
+      contentType: ContentType.json,
+      connectTimeout: 10000,
+      receiveTimeout: 10000,
+    ));
+    dio.interceptors.add(InterceptorsWrapper(onResponse: (Response response) {
+      return response;
+    }, onError: (DioError e) async {
+      return _onGetError(e);
+    }));
   }
 
   Future<HttpClientStatusCode> _checkConnectivity() async {
@@ -118,5 +40,86 @@ class HttpClient {
       clientStatusCode = HttpClientStatusCode.ERROR;
     }
     return clientStatusCode;
+  }
+
+  FutureOr<HttpClientModel> _onGetError(DioError e) async {
+    HttpClientStatusCode errorCode;
+    if (e.error.runtimeType == HttpException)
+      errorCode = await _checkConnectivity();
+    if (e.error.runtimeType == SocketException)
+      errorCode = HttpClientStatusCode.NO_INTERNET;
+    if (e.type == DioErrorType.RECEIVE_TIMEOUT)
+      errorCode = HttpClientStatusCode.SLOW_SERVER;
+    if (e.type == DioErrorType.CONNECT_TIMEOUT)
+      errorCode = HttpClientStatusCode.SLOW_INTERNET;
+    errorCode =
+        httpErrorVal[e.response?.statusCode] ?? HttpClientStatusCode.ERROR;
+    return HttpClientModel(error: errorCode);
+  }
+
+  T _parseJson<T>(String data, {bool isArray}) {
+    return isArray ? fromJsonArrayString<T>(data) : fromJsonString<T>(data);
+  }
+
+  Future<HttpClientModel> _buildClientModel<T>(
+      Response response, HttpClientModel responseModel, bool isArray) async {
+    try {
+      if (_success.contains(httpErrorVal[response.statusCode])) {
+        responseModel = HttpClientModel<T>(
+            error: HttpClientStatusCode.NONE,
+            model: await compute<String, T>(
+                (responseData) => _parseJson<T>(responseData, isArray: isArray),
+                response.data));
+      } else {
+        responseModel = HttpClientModel(
+          error:
+              httpErrorVal[response?.statusCode] ?? HttpClientStatusCode.ERROR,
+        );
+      }
+    } catch (e) {
+      responseModel = HttpClientModel(error: HttpClientStatusCode.ERROR);
+    }
+    return responseModel;
+  }
+
+  Future<HttpClientModel<T>> _baseGet<T>(String url,
+      {bool isArray = false}) async {
+    HttpClientModel<T> _responseModel;
+    Response response = await dio.get(url);
+    _responseModel =
+        await _buildClientModel<T>(response, _responseModel, isArray);
+    return _responseModel;
+  }
+
+  Future<HttpClientModel<T>> get<T>(String url) => _baseGet(url);
+
+  Future<HttpClientModel<T>> getAll<T>(String url) =>
+      _baseGet(url, isArray: true);
+
+  Future<HttpClientModel<T>> post<T>(T data, String url,
+      {bool isArray = false}) async {
+    HttpClientModel<T> _responseModel;
+    Response response = await dio.post(url, data: data);
+    _responseModel =
+        await _buildClientModel<T>(response, _responseModel, isArray);
+    return _responseModel;
+  }
+
+  Future<HttpClientModel<T>> put<T>(T data, String url,
+      {bool isArray = false}) async {
+    HttpClientModel<T> _responseModel;
+    Response response = await dio.put(url, data: data);
+    _responseModel =
+        await _buildClientModel<T>(response, _responseModel, isArray);
+    return _responseModel;
+  }
+
+  Future<HttpClientModel<T>> delete<T>(T data, String url,
+      {bool isArray = false}) async {
+    HttpClientModel<T> _responseModel;
+    Response response = await dio.delete(url, data: data);
+    _responseModel =
+        await _buildClientModel<T>(response, _responseModel, isArray);
+    return _responseModel;
   }
 }
